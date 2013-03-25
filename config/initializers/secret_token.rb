d0293c77ee0e39284af4d12bd1556c0a35846395
Christopher Bachicha <cbachich@gmail.com>
2013-03-24 19:17:31 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Americo::Application.config.secret_token = 'f0ca9d884f8f3c06e2530336ad20d84f74b2c5c3333107f1076f8d6fa3dcb2c680c3820d5ed635e908b8aaf7aa69ad071d9dd4c779485779984aba9c79096727'
