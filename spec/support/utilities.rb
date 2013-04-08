744a62c644f3b9cbad5841e89b5628fbffc3aa70
Christopher Bachicha <cbachich@gmail.com>
2013-04-08 08:32:56 -0600

    Update tests to support new page layout

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,15 @@
 include ApplicationHelper
 
+$company_save
 def company
-  company ||=
-    Company.create(title: "Test Company", logo_url: "http://s20.postimg.org/aqwv76rjh/americo_logo.jpg")
+  if defined? $company_save
+    $company_save
+  else
+    $company_save = Company.create(title: "Test Company", logo_url: "/images/americo_logo.jpg")
+    $company_save.description = Description.create(title: "Test Title", body: "Test Body")
+    $company_save.save
+    $company_save
+  end
 end
 
 def full_title(page_title)
