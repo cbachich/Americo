482049467525c923bdc9083aaebd1e26ddfe92d4
Christopher Bachicha <cbachich@gmail.com>
2013-06-07 08:36:04 -0600

    Remove the signup page for admins. Now an admin account will have to be made in the console.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Americo::Application.routes.draw do
 
-  devise_for :admins
+  devise_for :admins, :skip => [:registrations]
+  as :admin do
+    get 'admins/edit' => 'devise/registrations#edit', :as => 'edit_admin_registration'
+    put 'admins' => 'devise/registrations#update', :as => 'admin_registration'
+  end
 
   get "static_pages/home"
 
