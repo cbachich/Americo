23dae8c8b53f12b01a1daf2472ecd856524cb7d0
Christopher Bachicha <cbachich@gmail.com>
2013-08-01 09:56:07 -0600

    On initial start of website, create stub company database record

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
 
   def company
-    Company.first
+    build_stubs unless Company.exists?
+    @company ||= Company.first
   end
 
   # Returns the full title on a per-page basis
@@ -10,4 +11,67 @@ module ApplicationHelper
     base_title = "#{page_title} | #{base_title}" if !page_title.empty?
     base_title
   end
+
+  private
+
+    def build_stubs
+      new_company
+      new_services_sheet
+      new_about_sheet
+      add_stub_admin
+    end
+
+    def new_company
+      @company = Company.create(
+           title: "Company Name",
+           phone: "(555) 555-5555",
+             fax: "(555) 555-5555",
+        subtitle: "Quick description or quote",
+         summary: "Paragraphs describing the company.",
+           email: "example@company.com",
+         address: "555 Somewhere Lane",
+            city: "Someplace",
+           state: "CO",
+         zipcode: "55555"
+      )
+    end
+
+    def new_services_sheet
+      new_sheet "services", "Services"
+    end
+
+    def new_about_sheet
+      new_sheet "about", "About"
+    end
+
+    def new_sheet(name, title)
+      sheet = company.sheets.create(
+        name: name,
+        title: title,
+        subtitle: 
+          "Provide a very high level sentence describing the work done here."
+      )
+
+      sheet.pages.create(
+        name: "sample",
+        title: "Sample Page",
+        bullet_1: "Sample Bullet 1",
+        bullet_2: "Sample Bullet 2",
+        bullet_3: "Sample Bullet 3",
+        subtitle: "Title of the following paragraphs",
+        short_description: 
+          "This is a short one or two sentence description for the page.",
+        description: "A few paragraphs describing this page.",
+        reversed: false
+      )
+    end
+
+    def add_stub_admin
+      admin = Admin.new(
+        email: "admin@email.com",
+        password: "administrator",
+        password_confirmation: "administrator"
+      )
+      admin.save
+    end
 end
